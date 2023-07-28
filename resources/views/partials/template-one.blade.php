0f994ca3eedfc2e10e25f146c1db97609e92cfc2
mohammed-mortaja <sala.moh611@gmailcom>
2023-07-28 14:51:37 +0200

    update design

--- resources/views/partials/template-one.blade.php
+++ resources/views/partials/template-one.blade.php
@@ -9,40 +9,43 @@
     <div class="container-3-columns">
         <!-- الخبران الرئيسيان البارزان في هذا القالب -->
         @if (isset($categories[0]->blogs[0]))
+        <a href="{{ route('post_details', ['id' => $categories[0]->blogs[0]->id]) }}">
+
             <div class="column text-right pt-3 ">
                 <div style="width: 330px; height: 285px; background-image: url({{ asset('storage/images/blog/' . $categories[0]->blogs[0]->image ?? null) }});"
                     class="column position-relative ">
-                    <a href="{{ route('post_details', ['id' => $categories[0]->blogs[0]->id]) }}">
                         <div class="text text-right bottom-right text-white ">
                             <h6 class=" text-right"> {{ $categories[0]->blogs[0]->name }} </h6>
                             <span class="date "> {{ $categories[0]->blogs[0]->date }} </span>
                         </div>
-                    </a>
                 </div>
             </div>
+         </a>
+
         @endif
 
         @if (isset($categories[0]->blogs[1]))
+        <a href="{{ route('post_details', ['id' => $categories[0]->blogs[0]->id]) }}">
             <div class="column text-right pt-3">
                 <div style="width: 330px; height: 285px; background-image: url({{ asset('storage/images/blog/' . $categories[0]->blogs[1]->image ?? null) }});"
                     class="column position-relative ">
-                    <a href="{{ route('post_details', ['id' => $categories[0]->blogs[0]->id]) }}">
                         <div class="text text-right bottom-right text-white ">
                             <h6 class=" text-right"> {{ $categories[0]->blogs[1]->name ?? null }} </h6>
                             <span class="date "> {{ $categories[0]->blogs[1]->date ?? null }} </span>
                         </div>
-                    </a>
                 </div>
             </div>
+         </a>
+
         @endif
 
         <!-- هذا العمود يحتوي على 3 اخبار تابعة للتصنيف نفسه -->
         <div class="column pb-5">
             @if (isset($categories[0]->blogs[2]))
+            <a href="{{ route('post_details', ['id' => $categories[0]->blogs[0]->id]) }}">
                 <div class="box d-flex align-items-center py-3 ">
                     <div style="width: 100px; height: 75px; background-image: url( {{ asset('storage/images/blog/' . $categories[0]->blogs[2]->image ?? null) }} );"
                         class=" text-right">
-                        <a href="{{ route('post_details', ['id' => $categories[0]->blogs[0]->id]) }}"> </a>
                     </div>
                     <div class="text text-right  text-white mr-3">
                         <h6 class="text-right ">
@@ -51,13 +54,15 @@
                         <span class="date text-white-50">{{ $categories[0]->blogs[2]->date ?? null }} </span>
                     </div>
                 </div>
+               </a>
             @endif
 
             @if (isset($categories[0]->blogs[3]))
+            <a href="{{ route('post_details', ['id' => $categories[0]->blogs[0]->id]) }}">
                 <div class="box d-flex align-items-center  py-3  ">
                     <div style="width: 100px; height: 75px; background-image: url( {{ asset('storage/images/blog/' . $categories[0]->blogs[3]->image ?? null) }} );"
                         class="text-right">
-                        <a href="{{ route('post_details', ['id' => $categories[0]->blogs[0]->id]) }}"> </a>
+
                     </div>
                     <div class="text text-right  text-white mr-3 ">
                         <h6 class="text-right ">
@@ -66,12 +71,13 @@
                         <span class="date text-white-50"> {{ $categories[0]->blogs[3]->date ?? null }} </span>
                     </div>
                 </div>
+               </a>
             @endif
             @if (isset($categories[0]->blogs[4]))
+            <a href="{{ route('post_details', ['id' => $categories[0]->blogs[0]->id]) }}">
                 <div class="box d-flex align-items-center py-3   ">
                     <div style="width: 100px; height: 75px; background-image: url({{ asset('storage/images/blog/' . $categories[0]->blogs[4]->image ?? null) }} );"
                         class="text-right">
-                        <a href="{{ route('post_details', ['id' => $categories[0]->blogs[0]->id]) }}"> </a>
                     </div>
                     <div class="text text-right  text-white  mr-3">
                         <h6 class="text-right ">
@@ -80,6 +86,8 @@
                         <span class="date text-white-50"> {{ $categories[0]->blogs[4]->date ?? null }} </span>
                     </div>
                 </div>
+               </a>
+
             @endif
 
         </div>
